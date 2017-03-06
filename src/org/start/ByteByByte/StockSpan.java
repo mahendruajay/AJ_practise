8149717cb23b7d22110c75ac24dd77f15e085e6b
amahendru <amahendru@expedia.com>
2017-03-06 14:01:56 -0800

    adding new code

---
+++ src/org/start/ByteByByte/StockSpan.java
@@ -0,0 +1,9 @@
+package org.start.ByteByByte;
+
+public class StockSpan {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/org/start/ByteByByte/TreeToList.java
@@ -0,0 +1,41 @@
+package org.start.ByteByByte;
+
+public class TreeToList {
+
+    public static void main(String[] args) {
+
+    }
+
+    private class Node {
+        int value;
+        Node left;
+        Node right;
+    }
+
+    private Node concatenate(Node a, Node b) {
+        if (a == null) return b;
+        if (b == null) return a;
+
+        Node aEnd = a.left;
+        Node bEnd = b.left;
+
+        a.left = bEnd;
+        bEnd.right = a;
+        aEnd.right = b;
+        b.left = aEnd;
+        return a;
+    }
+
+    public Node treeToList(Node n) {
+        if (n == null) return n;
+        Node leftList = treeToList(n.left);
+        Node rightList = treeToList(n.right);
+        n.left = n;
+        n.right = n;
+
+        n = concatenate(leftList, n);
+        n = concatenate(n, rightList);
+        return n;
+    }
+
+}
